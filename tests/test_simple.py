f1286c5de3ba61ac48c3ab03d7e711df535f127e
Joseph C Wang <joequant@gmail.com>
2023-04-28 18:01:38 +0800

    add test

---
+++ tests/test_simple.py
@@ -0,0 +1,10 @@
+import unittest
+
+from tfi.data.bundle import Bundle
+
+class TestSimple(unittest.TestCase):
+    def test_simple(self):
+        bundle = Bundle()
+
+if __name__ == '__main__':
+    unittest.main()
