5031846dec0b7250ac81b98d5e153a20d73b71ba
Joseph C Wang <joequant@gmail.com>
2023-05-06 00:05:23 +0800

    add task interface

---
+++ src/tfi/data/task.py
@@ -0,0 +1,7 @@
+class Task:
+    def __init__(self):
+        pass
+
+    def run(self):
+        raise NotImplemented
+
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,45 +1,80 @@
 import os
 import unittest
 
+from overrides import override
 import tfi.data.reader
 import tfi.data.writer
 import tfi.data.bundle
+import tfi.data.task
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+class ReadTask(tfi.data.task.Task):
+    def __init__(self,
+                 database_: str,
+                 filename_: str,
+                 table_: str):
+        self.filename = filename_
+        self.table = table_
+        self.reader = tfi.data.reader.ReaderCSV()
+        self.writer = tfi.data.writer.WriterSql(
+            database_
+        )
+
+    @override
+    def run(self):
+        with open(
+                self.filename,
+                newline=''
+        ) as csvfile:
+            self.writer.drop_table(self.table, True)
+            b = self.reader.read_all(csvfile)
+            self.writer.write_all(b, table=self.table)
+
+class WriteTask(tfi.data.task.Task):
+    def __init__(self,
+                 database_: str,
+                 filename_: str,
+                 table_: str):
+        self.filename = filename_
+        self.table = table_
+        self.writer = tfi.data.writer.WriterCSV()
+        self.reader = tfi.data.reader.ReaderSql(
+            database_
+        )
+
+    @override
+    def run(self):
+        with open(
+                self.filename, 'w',
+                newline=''
+        ) as csvfile:
+            b = self.reader.read_all(self.table)
+            self.writer.write_all(b, csvfile)
 
 class TestSimple(unittest.TestCase):
     def test_simple(self):
         bundle = tfi.data.bundle.Bundle()
         reader = tfi.data.reader.Reader()
         writer = tfi.data.writer.Writer()
+        task = tfi.data.task.Task()
 
     def test_read_csv(self):
-        with open(
-                os.path.join(SCRIPT_DIR, 'eggs.csv'),
-                newline=''
-        ) as csvfile:
-            reader = tfi.data.reader.ReaderCSV()
-            writer = tfi.data.writer.WriterSql(
-                f'sqlite:///{SCRIPT_DIR}/database.db'
-            )
-            writer.drop_table('demo', True)
-            b = reader.read_all(csvfile)
-            writer.write_all(b, table='demo')
-"""
+        task = ReadTask(
+            f'sqlite:///{SCRIPT_DIR}/database.db',
+            os.path.join(SCRIPT_DIR, 'eggs.csv'),
+            'demo'
+        )
+        task.run()
+
     def test_write_csv(self):
-        with open(
-                os.path.join(
-                    SCRIPT_DIR, 'eggs.out.csv'
-                ),
-                'w',
-                newline=''
-        ) as csvfile:
-            reader = tfi.data.reader.ReaderSql(
-                f'sqlite:///{SCRIPT_DIR}/database.db'
-            )
-            b = reader.read_all('demo')
-            writer = tfi.data.writer.WriterCSV()
-            writer.write_all(b, csvfile)
-"""
+        task = WriteTask(
+            f'sqlite:///{SCRIPT_DIR}/database.db',
+            os.path.join(SCRIPT_DIR, 'eggs.out.csv'),
+            'demo'
+        )
+        task.run()
+
+
 if __name__ == '__main__':
     unittest.main()
