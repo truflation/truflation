4b062a570bf4ac6d71e4965427eb0ccd965969bb
david <daveaneo@gmail.com>
2023-05-11 11:46:13 -0600

    moved functionality to ingestor

--- src/tfi/data/data_pipeline.py
+++ src/tfi/data/data_pipeline.py
@@ -3,10 +3,13 @@
 import pandas as pd
 from typing import Optional
 from task import Task
-from reader import ReaderRest
+from reader import ReaderRest, Reader
 from writer import WriterCsv
 from data import Data, DataJson, DataFormat
 from transformer import Transformer
+import subprocess
+import os
+import shutil
 
 
 class Ingestor:
@@ -20,17 +23,80 @@ class Ingestor:
        Returns pandas
     '''
 
-    def __init__(self, reader):
-        pass
+    def __init__(self, name: str, reader: Reader):
         self.reader = reader
+        # Name of instance
+        self.name: str = ""
+        # data file => self.feather_file
+        # constraint file => self.tdda_file
+        # results file => self.results_file
 
-    def add_test(self, test):
+    def create_constraints(self):
+        '''
+        Automatically create constraints based on feather file, saved in self.feather_file
+        '''
+        if not self.name:
+            raise Exception('Reader must have a name before creating constraint.')
+        if os.path.isfile(f'{self.feather_file}'):
+            shutil.copyfile(f'{self.feather_file}', f'{self.feather_file}.bak')
+        cmd = f'tdda detect {self.feather_file} {self.tdda_file}'
+        # todo -- use other module for running commands that logs
+        subprocess.run(cmd)
+
+    def load_constraints(self, constraint_path):
+        assert os.path.isfile(constraint_path)
+        shutil.copyfile(constraint_path, f'{self.feather_file}')
+
+    def initialize(self):
         pass
+        # read data from reader, receive dataframe
+        df = self.reader.read_all()
 
+        # save dataframe to feather files => name.feather
+        self.save_df_to_feather(df)
+
+        # todo -- run through parser
+        # assume data already parsed
+
+        # Create constraints based on data
+        self.create_constraints()
+
+        # delete feather file
+        os.remove(f'{self.feather_file}')
+
+        # user can now edit .tdda file to further
 
     def ingest(self):
-        pass
-        # self.reader.import()
+        assert self.name != "", 'no name for ingestor'
+        assert os.path.isfile(f'{self.feather_file}'), f'no constraint file'
+
+        # read data from reader, receive dataframe
+        df = self.reader.read_all()
+
+        # save dataframe to feather files => name.feather
+        self.save_df_to_feather(df)
+
+        cmd = f'tdda detect {self.feather_file} {self.tdda_file} {self.results_file}'
+        subprocess.run(cmd)
+
+        # todo -- process results, which are in name.results
+
+        # todo -- manual check
+
+    def save_df_to_feather(self, df):
+        df.to_feather(f'{self.feather_file}')
+
+    @property
+    def feather_file(self):
+        return f'{self.name}.feather'
+
+    @property
+    def tdda_file(self):
+        return f'{self.name}.tdda'
+
+    @property
+    def results_file(self):
+        return f'{self.name}.results'
 
 
 class DataPipeline:
@@ -51,14 +117,17 @@ class DataPipeline:
         self.exports = dict()
 
     # we should be able to assume that cleaned data exists in each of these ingestors, once we run it
-    def add_ingestor(self, ingestor_name: str, ingestor: Ingestor) -> None:
+    def create_ingestor(self, ingestor_name: str, reader: Reader) -> None:
         '''
         add self-contained imports, cleaning, and QA, yielding constrained data
         '''
-        self.ingestors[ingestor_name] = ingestor
+        assert ingestor_name not in [name for name in self.ingestors.keys()], "ingestor name already used"
+        assert not self.ingestors.get(ingestor_name, None), "ingestor name already used"
+        self.ingestors[ingestor_name] = Ingestor(ingestor_name, reader)
 
     # todo -- it should be clear how data is fed into the transformer
-    #
+    #      -- clearly map ingestors (feather files) to transformers
+    # will there be 1 big transformer function or multiple? Likely one
     def add_transformer(self, transformer_name: str, transformer: Transformer) -> None:
         '''
         this takes the data in the data pipeline and processes it
@@ -76,19 +145,18 @@ class DataPipeline:
         processes whole pipeline
         '''
         for name, ingestor in self.ingestors.items():
-            pass
-            # res =  # ingestor.ingest
-            # if res:
-            #     #     error
-
+            res = ingestor.ingest()
+            if res:
+                raise Exception(f"error ingesting {name}")
         for transformer in self.transformers.keys():
             pass
             # we need to be able to cleanly label and process data
 
 
         # run QA
+        pass
 
         # export data
-
+        pass
 
 foo = DataPipeline()
--- src/tfi/data/reader.py
+++ src/tfi/data/reader.py
@@ -9,7 +9,6 @@ import os
 from typing import Optional
 import pandas
 import requests
-import subprocess
 
 from sqlalchemy import create_engine
 from tfi.data.data import Data, DataPandas, DataJson
@@ -20,20 +19,13 @@ class Reader:
     """
     def __init__(self):
         pass
-        # we might use the convention of name.tdda, name.db for file paths
-        # The types of data, confined to MariaDB types
-        self.tdda_file: str = ""
-        # Dataframe of processed data
-        self.data_file: str = ""
         # Function to parse or transform data received form API to wanted format
         self.parser: object = None
-        # Name of instance
-        self.name: str = ""
 
     def authenticate(self, token):
         pass
 
-    # todo -- @joseph this should return a standard format
+    # todo -- @joseph this should return a standard format. I will assume a pandas dataframe
     def read_all(
             self,
             *args,
@@ -54,38 +46,6 @@ class Reader:
             **kwargs
     ) -> Optional[Data]:
         return None
-
-    def create_constraints(self):
-        pass
-        if not self.name:
-            raise Exception('Reader must have a name before creating constraint.')
-        elif not self.data:
-            raise Exception(f'{self.name} (reader) must have a data before creating constraint.')
-
-        cmd = f'tdda detect {self.data_file} {self.name}.tdda'
-        # todo -- use other module for running commands that logs
-        subprocess.run(cmd)
-
-    def load_constraints(self, constraint_path):
-        self.tdda_file = constraint_path
-
-    def initialize(self):
-        pass
-        # todo -- read data
-        # todo -- run through parser
-        # todo -- store in self.name.datatype
-        # self.create_constraints()
-        self.create_constraints()
-        # user can now edit .tdda file to further
-
-    def ingest(self):
-        assert self.tdda_file != "" and self.data_file != ""
-        # todo -- read
-        cmd = f'tdda detect {self.data_file} constraints.tdda {self.name}.results'
-        subprocess.run(cmd)
-        # todo -- process results
-        # todo -- manual check
-        pass
 
 class ReaderCsv(Reader):
     def __init__(self):
