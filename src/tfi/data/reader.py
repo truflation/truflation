76f8255a2a6443c9daf358b9d4246a70e1f92d85
david <daveaneo@gmail.com>
2023-05-11 09:41:21 -0600

    added constraints and more to reader

--- src/tfi/data/reader.py
+++ src/tfi/data/reader.py
@@ -2,10 +2,14 @@
 Reader
 """
 
+# Constarints library
+# https://tdda.readthedocs.io/en/latest/constraints.html
+
 import os
 from typing import Optional
 import pandas
 import requests
+import subprocess
 
 from sqlalchemy import create_engine
 from tfi.data.data import Data, DataPandas, DataJson
@@ -16,10 +20,20 @@ class Reader:
     """
     def __init__(self):
         pass
+        # we might use the convention of name.tdda, name.db for file paths
+        # The types of data, confined to MariaDB types
+        self.tdda_file: str = ""
+        # Dataframe of processed data
+        self.data_file: str = ""
+        # Function to parse or transform data received form API to wanted format
+        self.parser: object = None
+        # Name of instance
+        self.name: str = ""
 
     def authenticate(self, token):
         pass
 
+    # todo -- @joseph this should return a standard format
     def read_all(
             self,
             *args,
@@ -40,6 +54,38 @@ class Reader:
             **kwargs
     ) -> Optional[Data]:
         return None
+
+    def create_constraints(self):
+        pass
+        if not self.name:
+            raise Exception('Reader must have a name before creating constraint.')
+        elif not self.data:
+            raise Exception(f'{self.name} (reader) must have a data before creating constraint.')
+
+        cmd = f'tdda detect {self.data_file} {self.name}.tdda'
+        # todo -- use other module for running commands that logs
+        subprocess.run(cmd)
+
+    def load_constraints(self, constraint_path):
+        self.tdda_file = constraint_path
+
+    def initialize(self):
+        pass
+        # todo -- read data
+        # todo -- run through parser
+        # todo -- store in self.name.datatype
+        # self.create_constraints()
+        self.create_constraints()
+        # user can now edit .tdda file to further
+
+    def ingest(self):
+        assert self.tdda_file != "" and self.data_file != ""
+        # todo -- read
+        cmd = f'tdda detect {self.data_file} constraints.tdda {self.name}.results'
+        subprocess.run(cmd)
+        # todo -- process results
+        # todo -- manual check
+        pass
 
 class ReaderCsv(Reader):
     def __init__(self):
