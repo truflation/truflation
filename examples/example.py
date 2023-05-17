90eda067674b478ad36a2430e2f0952f86c6ec7e
david <daveaneo@gmail.com>
2023-05-17 08:56:19 -0600

    meeting notes.

--- examples/example.py
+++ examples/example.py
@@ -9,12 +9,27 @@
 # -- one ingestor should create one table (updateable) -- maybe initial setup is, like creating infrastructure tables, can bypass this rule
 # --
 
+# Discussion points
+#   Reader, Writer not passed in, using JSON w/ csv/API/excel
+#   Considering multiple ins of different types and multiple outs (cameron wants 1)
+#   Scaling to many pipelines --
+#      -- what would that look like?
+#      -- would we still use Docker or would we create a library?
+#      --> 3 docker images: modularized pipeline, in-parallel monolith, everything (private)
+#   Moving to standard pipeline
+#   How to make it versatile so it could load all data across all 'pipelines' first, or do all 'pipelines' fully
+
+# Task --- import CPIH using this system
+# Task --- David, do the generalized pipeline using Joseph's updates and feeding in data generally
+# Task --- Joseph, consider multiple inputs (of different types) and outputs (to different dbs  and outputting different rows/structures)
+# Task --- Jospeh, consider a better way of scaling -- compared to our current scaling solution.
 
 import sys
 from tfi.data.validator import Validator
 from tfi.data.task import Task
 from tfi.data.loader import Loader
 from tfi.data.data import DataPandas, DataFormat
+
 
 class AddHours(Task):
     def __init__(self, reader, writer):
@@ -31,11 +46,11 @@ class AddHours(Task):
         res_df["hours coding"] = df1["hours coding"].add(df2["hours coding"])
         self.writer.write_all(DataPandas(res_df), key="hours_coding")
 
+
 class CalculateDeveloperHours(Task):
     def __init__(self, reader, writer):
         super().__init__(reader, writer)
         self.data = ["developer_hours", "developer_hours2"]
-
         self.loader = Loader(self.reader, "cache")
         self.validator = Validator(
             "cache", "cache"
