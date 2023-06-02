20f1c6c4fbf683d862a574652a0261f7f62d8f54
Joseph C Wang <joequant@gmail.com>
2023-06-02 14:53:53 +0800

    fix f1.py

--- examples/f1.py
+++ examples/f1.py
@@ -1,16 +1,16 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # http://ergast.com/api/f1/2004/1/results.json
 
-from truflation.data.reader import ReaderRest
+from truflation.data.connector import ConnectorRest
 from truflation.data.task import Task
 
 class F1Task(Task):
     def __init__(self):
-        self.reader = ReaderRest('http://ergast.com/api')
+        self.reader = ConnectorRest('http://ergast.com/api')
     def run(self):
         b = self.reader.read_all('f1/2004/1/results.json')
-        print(b.get())
+        print(b)
 
 if __name__ == '__main__':
     f1 = F1Task()
