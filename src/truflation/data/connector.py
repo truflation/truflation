69efa8ea632eadb2174441696ac4554ea78fa246
Joseph C Wang <joequant@gmail.com>
2023-06-12 11:06:26 +0800

    add databae handle

--- src/truflation/data/connector.py
+++ src/truflation/data/connector.py
@@ -15,7 +15,6 @@ import sqlalchemy
 from sqlalchemy.sql import text
 from sqlalchemy import create_engine, Table, MetaData
 
-
 class Connector:
     """
     Base class for Import
@@ -312,3 +311,13 @@ def connector_factory(connector_type: str) -> Optional[Connector]:
             connector_type.startswith('pybigquery'):
         return ConnectorSql(connector_type)
     return None
+
+def get_database_handle(db_type='mariadb+pymysql'):
+    DB_USER = os.environ.get('DB_USER', None)
+    DB_PASSWORD = os.environ.get('DB_PASSWORD', None)
+    DB_HOST = os.environ.get('DB_HOST', '127.0.0.1')
+    DB_PORT = int(os.environ.get('DB_PORT', None))
+    DB_NAME = os.environ.get('DB_NAME', None)
+
+    return f'{db_type}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
