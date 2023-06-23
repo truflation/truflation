88d65eac3eef5084d79393878a6d7553e2c2d09d
david <daveaneo@gmail.com>
2023-06-23 10:23:43 -0600

    formatting

--- src/truflation/data/util.py
+++ src/truflation/data/util.py
@@ -1,4 +1,6 @@
 from datetime import datetime
 
+
 def get_today_string():
-    return  datetime.utcnow().date().strftime('%Y-%m-%d')
+    """ Returns todays' date in format year-month-day; Example:2023-06-23 """
+    return datetime.utcnow().date().strftime('%Y-%m-%d')
