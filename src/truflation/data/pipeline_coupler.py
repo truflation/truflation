0d422c37c004f64b0b32b29973565111b5c84b42
Joseph C Wang <joequant@gmail.com>
2023-07-28 09:42:33 +0800

    move pipeline_details inside scheduler loop
    Move pipeline_details inside scheduler loop.  Otherwise pipeline_details
    would not be seeing dynamic changes to the details list.  This will
    happen when requesting that the details read the current date, and
    without the fix, the date will be fixed to the old dates.

--- src/truflation/data/pipeline_coupler.py
+++ src/truflation/data/pipeline_coupler.py
@@ -14,37 +14,41 @@ import sys
 import time
 import logging
 import json
-from apscheduler.schedulers.background import BackgroundScheduler
-from truflation.data.pipeline import Pipeline
-from truflation.data.pipeline_details import PipeLineDetails
-from docopt import docopt
 import importlib
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from docopt import docopt
 from pytz import utc
 
+from truflation.data.pipeline import Pipeline
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 
-def ingest(pipeline_details: PipeLineDetails):
+def ingest(module_list: list, config: dict):
     """
        Instantiates a Pipeline object and initiates the ingest process.
 
        Parameters
        ----------
-       pipeline_details : PipeLineDetails
-           An instance of PipeLineDetails that holds the pipeline's specific details.
+       module_list: List of modules to run
 
        Returns
        -------
        None
        """
-    logging.debug(pipeline_details)
-    if hasattr(pipeline_details, '__iter__'):
-        [ Pipeline(detail).ingest() for detail in pipeline_details ]
-    else:
-        Pipeline(pipeline_details).ingest()
+    pipeline_details_list = []
+    for module in module_list:
+        if hasattr(module, 'get_details_list'):
+            pipeline_details_list.extend(module.get_details_list(**config))
+        elif hasattr(module, 'get_details'):
+            pipeline_details_list.append(module.get_details(**config))
+        else:
+            raise Exception("get_details not found in supplied module,")
 
+    logging.debug(pipeline_details_list)
+    [ Pipeline(detail).ingest() for detail in pipeline_details_list ]
 
-def main(pipeline_details: PipeLineDetails, cron_schedule=None):
+def main(module_list: list, cron_schedule=None):
     """
     Main driver function to set up the pipeline and start the scheduler.
 
@@ -59,17 +63,19 @@ def main(pipeline_details: PipeLineDetails, cron_schedule=None):
     scheduler = BackgroundScheduler(timezone=utc)
 
     if cron_schedule is None:
-        if hasattr(pipeline_details, '__iter__'):
-            cron_schedule = pipeline_details[0].cron_schedule
+        if hasattr(module_list[0], 'get_details_list'):
+            cron_schedule = module_list[0].get_details_list(**config)[0].cron_schedule
         else:
-            cron_schedule = pipeline_details.cron_schedule
+            cron_schedule = module_list[0].get_details(**config).cron_schedule
 
     # Add job based off of cron_schedule in pipeline_details
-    job = scheduler.add_job(ingest,  'cron', **cron_schedule, args=[pipeline_details])
+    job = scheduler.add_job(ingest,
+                            'cron',
+                            **cron_schedule, args=[module_list, config])
     scheduler.start()
 
     # Print job and pipeline details so we know it is functioning
-    print(f'Scheduling ingestor pipeline {pipeline_details}')
+    print('Scheduling ingestor pipelines')
     print(f' --> {job}')
 
     # Runs an infinite loop
@@ -85,7 +91,7 @@ async def load_path(
         config = {}
 
     # Dynamically import and run module, pipeline_details
-    pipeline_details_list = []
+    module_list = []
     for file_path in file_path_list:
         module_name = 'my_pipeline_details'
         spec = importlib.util.spec_from_file_location(module_name, file_path)
@@ -94,14 +100,9 @@ async def load_path(
 
         module = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(module)
+        module_list.append(module)
 
-        if hasattr(module, 'get_details_list'):
-            pipeline_details_list.extend(module.get_details_list(**config))
-        elif hasattr(module, 'get_details'):
-            pipeline_details_list.append(module.get_details(**config))
-        else:
-            raise Exception("get_details not found in supplied module,")
-    main(pipeline_details_list, cron_schedule)
+    main(module_list, cron_schedule)
 
 if __name__ == '__main__':
     # Get file_path from argument
@@ -114,7 +115,6 @@ if __name__ == '__main__':
     if args.get('--cron') is None:
         asyncio.run(load_path(file_path, None, config))
     else:
-        with open(args['--cron']) as cronh:
+        with open(args['--cron'], encoding='utf-8') as cronh:
             cron_schedule = json.load(cronh)
             asyncio.run(load_path(file_path, cron_schedule, config))
-
