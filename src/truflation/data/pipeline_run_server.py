7612af59e03b806f999b33d69e084fa14039bd76
Joseph C Wang <joequant@gmail.com>
2023-08-27 10:47:23 +0800

    refactor pipeline_run_server

--- src/truflation/data/pipeline_run_server.py
+++ src/truflation/data/pipeline_run_server.py
@@ -17,46 +17,14 @@ from sanic.response import text, json
 
 from truflation.data.pipeline import Pipeline
 from truflation.data.connector import cache_
+import pipeline_run_direct
 
 async def load_path(file_path_list: List[str] | str,
                     debug: bool, dry_run: bool,
                     config: dict | None = None):
-    """
-    Dynamically import and run module, pipeline_details
-    """
-    return_value = []
-
-    if config is None:
-        config = {}
-    cache_.clear()
-    # convert strings to lists
-    if isinstance(file_path_list, str):
-        file_path_list = file_path_list.split(" ")
-
-    for file_path in file_path_list:
-        if debug:
-            print('debugging')
-            logging.basicConfig(level=logging.DEBUG)
-        module_name = 'my_pipeline_details'
-        spec = importlib.util.spec_from_file_location(module_name, file_path)
-        if spec is None or spec.loader is None:
-            raise Exception(f"{file_path} does not exist as a module.")
-        module = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(module)
-
-        if hasattr(module, 'get_details_list'):
-            return_value.extend([
-                Pipeline(detail).ingest(dry_run)
-                for detail in module.get_details_list(**config)
-            ])
-        elif hasattr(module, 'get_details'):
-            pipeline_details = module.get_details(**config)
-            my_pipeline = Pipeline(pipeline_details)
-            return_value.append(my_pipeline.ingest(dry_run))
-        else:
-            raise Exception("get_details not found in supplied module,")
-
-    return return_value
+    return pipeline_run_direct.load_path(
+        file_path_list, debug, dry_run, config
+    )
 
 app = Sanic("PipelineServerApp")
 args = docopt(__doc__)
