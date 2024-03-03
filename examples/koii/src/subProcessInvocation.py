f7ffbd580b7644e34f6cbb306c1d22602a9d88f9
letteldream <letteldream@gmail.com>
2024-03-03 06:17:03 -0500

    feat: update Koii data ingestion script for new task ID

--- examples/koii/src/subProcessInvocation.py
+++ examples/koii/src/subProcessInvocation.py
@@ -2,6 +2,7 @@ import subprocess
 import time
 import pandas as pd
 import json
+from datetime import datetime
 
 def run_js_script(script_path):
     try:
@@ -25,7 +26,7 @@ def fetch_data_and_convert_to_csv(json_file, csv_file):
         json_data = json.load(file)
 
     # Prepare the CSV header
-    csv_header = 'CID'
+    csv_header = 'CID,Timestamp,Timestamp (Human Readable)'
     cities = ['Denver', 'Boston', 'Brooklyn', 'Chicago', 'Seattle', 'Miami', 'Washington, D.C.', 'Los Angeles']
     categories = ['Economy', 'Compact', 'Intermediate', 'Standard SUV']
 
@@ -36,18 +37,23 @@ def fetch_data_and_convert_to_csv(json_file, csv_file):
     csv_data = f"{csv_header}\n"
 
     # Process each round
-    for key, value in json_data.items():
-        round_data = value.get("locationSummary", [])
-        csv_row = [key]
+    for round_data in json_data['1']:
+        round_id = round_data['id']
+        timestamp = round_data.get("data", {}).get("timestamp", "")
+        
+        # Convert timestamp to datetime object and format it
+        human_readable_timestamp = datetime.fromtimestamp(timestamp / 1000).strftime('%Y-%m-%d %H:%M:%S')
+        
+        location_summary = round_data.get("data", {}).get("locationSummary", [])
+        csv_row = [round_id, timestamp, human_readable_timestamp]
 
         for city in cities:
-            city_data = next((item for item in round_data if item["location"] == city), {}).get("data", {})
+            city_data = next((item["data"] for item in location_summary if item["location"] == city), {})
             csv_row.extend([city_data.get(category, "") for category in categories])
 
         csv_data += ','.join(map(str, csv_row)) + '\n'
 
     # Write CSV data to a file
-    print(csv_data)
     with open(csv_file, 'w') as file:
         file.write(csv_data)
 
@@ -57,7 +63,7 @@ def fetch_data_and_convert_to_csv(json_file, csv_file):
 
 def main():
     script_path = './fetchAvgData.js'  # Path to your JS script
-    json_file_path = '../output/historicData.json'  # Path to the output JSON file
+    json_file_path = '../output/submissionData.json'  # Path to the output JSON file
     csv_file_path = '../output/csv.csv'
 
     while True:
