ae4589a1fcf414a982bbb3dd7d6986b7175e88cb
letteldream <letteldream@gmail.com>
2024-04-01 07:39:28 -0500

    add scaffold metadata handler unit test

--- tests/metadata_handler_test_local.py
+++ tests/metadata_handler_test_local.py
@@ -2,7 +2,7 @@ import os
 from tokenize import String
 from unittest import mock
 import pytest
-from sqlalchemy import Column, DateTime, Integer, create_engine, func, select, MetaData, Table
+from sqlalchemy import DATETIME, VARCHAR, Column, create_engine, func, select, MetaData, Table
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.exc import OperationalError
@@ -10,19 +10,19 @@ from truflation.data._metadata_handler import _MetadataHandler
 
 mock_frequency = '''[
     {
-        "index": "com_aaa_us_gasoline",
+        "index": "mock_category_mock_name",
         "frequency": "Daily",
         "other": "No",
         "exact": 1
     }
 ]'''
 
-env = {"DB_USER": "root", "DB_PASSWORD": "password12321", "DB_HOST": "api-test.truflation.io", "DB_PORT": "3306", "DB_NAME": "timeseries", "USE_METADATA_HANDLER": "1"}
+env = {"USE_METADATA_HANDLER": "1"}
 
 @pytest.fixture
 def handler():
 
-    # engine = create_engine('sqlite:///:memory:')
+    engine = create_engine('sqlite:///:memory:')
     # SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
     # metadata = MetaData()
@@ -31,34 +31,51 @@ def handler():
          mock.patch("os.path.exists", return_value=True), \
          mock.patch("builtins.open", mock.mock_open(read_data=mock_frequency)):
          
-         
-         handler = _MetadataHandler()
-        #  handler.metadata = metadata
-
-        #  handler._metadata = Table(
-        #     '_metadata', metadata,
-        #     Column('id', Integer, primary_key=True),
-        #     Column('table_name', String ),
-        #     Column('_key', String),
-        #     Column('value', String),
-        #     Column('value_type', String),
-        #     Column('created_at', DateTime),
-        #     Column('updated_at', DateTime),
-        #  )
-
+         handler = _MetadataHandler(engine)
     #      metadata.create_all(engine)
 
     # yield handler
     # engine.dispose()
     return handler
 
+# def test_create_mock_index_table(handler):
+
+#         # self._metadata = Table(
+#         #     self.table,
+#         #     self.metadata,
+#         #     Column('table_name', VARCHAR(255)),
+#         #     Column('_key', VARCHAR(255)),
+#         #     Column('value', VARCHAR(255)),
+#         #     Column('value_type', VARCHAR(255)),
+#         #     Column('created_at', DATETIME),
+#         #     Column('updated_at', DATETIME),
+#         # )
+        
+#         # try:
+#         #     # Create the table
+#         #     self.metadata.create_all(self.engine)
+#         #     ic(f'Table {self.table} created successfully.')
+            
+#         # except OperationalError as err:
+#         #     ic(f'An error occurred while creating {self.table} table: {err}')
+
+#     self.engine = engine
+
+#     _mockTable = Table(
+#         'mock_category_mock_name',
+#         MetaData(),
+#         Column('date', VARCHAR(255)),
+#         Column('value', VARCHAR(255)),
+#         Column('created_at', DATETIME),
+#     )
+
 def test_get_frequency(handler):
-    frequency = handler.get_frequency_data("com_aaa_us_gasoline")
+    frequency = handler.get_frequency_data("mock_category_mock_name")
     assert frequency['frequency'] == 'Daily'
     assert frequency['exact'] == 1
 
 def test_validata_table(handler):
-    assert handler.validate_table('com_aaa_us_gasoline') is True
+    assert handler.validate_table('mock_category_mock_name') is True
     assert handler.validate_table('__metadata__') is False
 
 def test_create_table(handler):
@@ -68,12 +85,12 @@ def test_create_table(handler):
 def test_add_index(handler):
     
     print(f'engine: {handler.engine}');
-    handler.add_index("com_aaa_us_gasoline");
+    handler.add_index("mock_category_mock_name");
 
     with handler.engine.connect() as conn:
         try:
             _metadata = Table(handler.table, handler.metadata, autoload_with = handler.engine)
-            query = select(_metadata).where(_metadata.c.table_name == "com_aaa_us_gasoline").where(_metadata.c._key == "last_update")
+            query = select(_metadata).where(_metadata.c.table_name == "mock_category_mock_name").where(_metadata.c._key == "last_update")
             result = conn.execute(query).fetchone()
 
             count_query = select(func.count()).select_from(_metadata)
@@ -81,10 +98,10 @@ def test_add_index(handler):
             assert count_result == 6
 
             assert result is not None
-            assert result[0] == "com_aaa_us_gasoline"
+            assert result[0] == "mock_category_mock_name"
             
 
-            query = select(_metadata).where(_metadata.c.table_name == "com_aaa_us_gasoline")
+            query = select(_metadata).where(_metadata.c.table_name == "mock_category_mock_name")
             result = conn.execute(query).fetchall()
 
             print(f'result: {result}')
@@ -106,11 +123,11 @@ def test_add_index(handler):
 
 def test_insert_row_manually(handler):
     
-    handler.insert_row("com_aaa_us_gasoline", "last_update", "2000-1-1" , "string" )
+    handler.insert_row("mock_category_mock_name", "last_update", "2000-1-1" , "string" )
     with handler.engine.connect() as conn:
         try:
             _metadata = Table(handler.table, handler.metadata, autoload_with = handler.engine)
-            query = select(_metadata).where(_metadata.c.table_name == "com_aaa_us_gasoline").where(_metadata.c._key == "last_update")
+            query = select(_metadata).where(_metadata.c.table_name == "mock_category_mock_name").where(_metadata.c._key == "last_update")
             result = conn.execute(query).fetchone()
             assert result[2] == '2000-1-1'
         except OperationalError as err:
