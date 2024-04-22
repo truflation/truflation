8d5fc3f9df0c11d00f859e080fc679a374b25c1b
Joseph C Wang <joequant@gmail.com>
2024-04-22 21:55:36 +0800

    refactor signer and add eip719 signer

--- src/truflation/data/rt_http_server.py
+++ src/truflation/data/rt_http_server.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 import os
+import traceback
 from datetime import datetime
 import ujson
 
-from sanic import Sanic
+from sanic import Sanic, exceptions
 from sanic.response import json, text
-from jwcrypto import jwk, jwt
+
 from sqlalchemy import text, select
 
 from dotenv import load_dotenv
@@ -13,12 +14,26 @@ from icecream import ic
 from truflation.data.connector import connector_factory
 from truflation.data.source_details import SourceDetails
 from truflation.data.connector import get_database_handle
+from truflation.data.signer import Signer
 
 load_dotenv()
 
 app = Sanic('rt_http_server')
-BASE_DIRECTORY = os.path.join(os.path.dirname(os.path.abspath(__file__)), "json")
+BASE_DIRECTORY = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), "json"
+)
 LIMIT=5
+signer = Signer.factory(
+    os.environ.get('RT_HTTP_SERVER_SIGNER', 'jwt')
+)
+
+def set_base_directory(base_directory: str):
+    global BASE_DIRECTORY
+    BASE_DIRECTORY = base_directory
+
+def set_signer(sign_string, *args, **kwargs):
+    global signer
+    signer = Signer.factory(sign_string, *args, **kwargs)
 
 def is_valid_file_path(file_path):
     if not file_path.startswith(BASE_DIRECTORY):
@@ -39,23 +54,19 @@ def convdate(timestamp):
         int(timestamp)/1000
     ))
 
-@app.route("/auth")
-async def auth(request):
-    pubkey = os.environ.get('RT_PUB_KEY')
-    if pubkey is not None:
-        key = jwk.JWK.from_pem(pubkey.encode('utf-8'))
-        return json(key)
-    return json({})
+@app.route("/auth-info")
+async def auth_info(request):
+    return json(signer.auth_info())
 
 def sign_json(payload):
-    privkey = os.environ.get('RT_PRIV_KEY')
-    if privkey is not None:
-        key = jwk.JWK.from_pem(privkey.encode('utf-8'))
-        token = jwt.JWT(header={'alg': 'ES512'},
-                        claims=payload)
-        token.make_signed_token(key)
-        jresponse = json(payload | { 'j': token.serialize() })
-        jresponse.headers['Authorization'] = 'Bearer ' + token.serialize()
+    payload_sign = signer.preprocess(payload)
+    signature = signer.signature(payload_sign)
+    if signature is not None:
+        if isinstance(signature, dict):
+            jresponse = json(payload_sign | signature)
+        if isinstance(signature, str):
+            jresponse = json(payload_sign | { 'i': signature })
+            jresponse.headers['Authorization'] = 'Bearer ' + signature
         return jresponse
     return json(payload)
 
@@ -116,22 +127,24 @@ https://www.tradingview.com/charting-library-docs/latest/connecting_data/UDF/
         }
         return sign_json(j)
     except Exception as e:
-        ic(e)
+        print(e)
+        traceback.print_tb(e.__traceback__)
         return json({
             's': 'error'
         })
 
 @app.route("/data/<filename:[A-z][A-z0-9_.]*>")
 async def get_file_contents(_, filename):
+
+    # Construct the full path to the file
+    file_path = os.path.join(BASE_DIRECTORY, filename)
+    if not is_valid_file_path(file_path):
+        return json({"error": "Invalid file"}, status=404)
     try:
-        # Construct the full path to the file
-        file_path = os.path.join(BASE_DIRECTORY, filename)
-        if not is_valid_file_path(file_path):
-            return text("Invalid file", status=403)
         with open(file_path, "r", encoding='utf-8') as file:
             file_type = get_file_type(filename)
             if file_type == 'json':
-                return sign_json(ujson.load(file))
+                return json(ujson.load(file))
             contents = file.read()
             response = text(contents)
             response.headers["Content-Type"] = f"application/{file_type}"
