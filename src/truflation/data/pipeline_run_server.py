09dae7e0b9546fa1401b767f71f0564a207015d1
Joseph C Wang <joequant@gmail.com>
2024-05-12 04:26:29 +0800

    fix fastapi bug in run server

--- src/truflation/data/pipeline_run_server.py
+++ src/truflation/data/pipeline_run_server.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 Usage:
   pipeline_run_server.py <details_path> ... [--debug] [--dry_run] [--port=<n>]
@@ -8,25 +7,28 @@ Arguments:
   details_path     the relative path to the pipeline details module
 """
 
-"""
-Issues in pipeline_run_server.py:
-
-The pipeline_run_server.py script isn't currently taking advantage of asynchronous features (async/await).
-Although the Sanic web framework it uses supports asynchronous capabilities, the script itself isn't making use of them.
-To make the most of asynchronous operations, consider using await for tasks that might cause delays, like reading from disk or network operations.
-
-"""
-
-import importlib
-import logging
 from typing import List
 from docopt import docopt
 from fastapi import FastAPI, Request
 import uvicorn
 
-from truflation.data.pipeline import Pipeline
 from truflation.data.connector import cache_
 import pipeline_run_direct
+
+"""
+Issues in pipeline_run_server.py:
+
+The pipeline_run_server.py script isn't currently taking
+advantage of asynchronous features (async/await).
+
+Although the Sanic web framework it uses supports asynchronous
+capabilities, the script itself isn't making use of them.
+To make the most of asynchronous operations, consider using
+await for tasks that might cause delays, like reading from disk
+or network operations.
+
+"""
+
 
 async def load_path(file_path_list: List[str] | str,
                     debug: bool, dry_run: bool,
@@ -46,7 +48,7 @@ async def hello_world():
 
 @app.get("/{output}")
 async def test(output: str, request: Request):
-    query_params = { key: value[0] for key, value in request.query_params.items() }
+    query_params = dict(request.query_params.items())
     filelist = [ item for item in args['<details_path>'] if '=' not in item ]
     config = { item.split('=')[0]: item.split('=')[1] \
                for item in args['<details_path>'] if '=' in item }
@@ -54,7 +56,6 @@ async def test(output: str, request: Request):
     await load_path(
         filelist,
         args['--debug'], args['--dry_run'], {**config, **query_params})
-    print(cache_.cache_data.keys())
     return cache_.get(output)
 
 if __name__ == '__main__':
